5f8263144bd3fe2d509307a00db958273524a236
Andresian <andresiandd@gmail.com>
2020-01-31 19:57:53 +0300

    Update dust.gd

--- base/entities/scripts/dust.gd
+++ base/entities/scripts/dust.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 onready var anims = $AnimationPlayer
+var offset = float(19)
 
 func _ready():
 	pass
 	
-func init(pose, posy):
-	global_transform = pose
-	global_position = Vector2(posy.x, posy.y - float(19))
+func init(o_tr, o_pos):
+	global_transform = o_tr
+	global_position = Vector2(o_pos.x, o_pos.y - offset)
 	anims.play("Play")
 
 func destroy():
