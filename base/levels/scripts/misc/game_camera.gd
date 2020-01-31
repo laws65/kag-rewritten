577f48188d230091bc25b9a87e4adb3b89cbbf4d
andreyrk <dev.andreyrk@gmail.com>
2020-01-31 07:27:01 -0300

    Add zoom control

--- base/levels/scripts/misc/game_camera.gd
+++ base/levels/scripts/misc/game_camera.gd
@@ -5,3 +5,12 @@ var target
 func _process(delta):
 	if target:
 		global_position = target.global_position
+
+func _input(event):
+	if event.is_pressed() and event is InputEventMouseButton:
+		if event.button_index == BUTTON_WHEEL_UP:
+			zoom.y = clamp(zoom.y - 0.05, 0.25, 1)
+			zoom.x = zoom.y
+		if event.button_index == BUTTON_WHEEL_DOWN:
+			zoom.y = clamp(zoom.y + 0.05, 0.25, 1)
+			zoom.x = zoom.y
